b75da441478e03ff60dc93e7f46ed1e62edf8b09
Luis H. P. Mendes <luishpmendes@gmail.com>
2018-05-19 21:58:08 -0300

    Updating result validator.
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

--- ra117801/codigo/resultValidator.cpp
+++ ra117801/codigo/resultValidator.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <vector>
 
 #ifndef NIL
@@ -29,108 +30,36 @@ struct Sets {
     vector <int> rank;
 };
 
+bool edgeComparator (Edge e, Edge f) {
+    if (e.u == f.u) {
+        return e.v < f.v;
+    }
+
+    return e.u < f.u;
+}
+
+bool edgeWeightComparator (Edge a, Edge b) {
+    return (a.w < b.w);
+}
+
+bool conflictingPairComparator (ConflictingPair a, ConflictingPair b) {
+    if (a.e.u == b.e.u) {
+        if (a.e.v == b.e.v) {
+            if (a.f.u == b.f.u) {
+                return a.f.v < b.f.v;
+            }
+
+            return a.f.u < b.f.u;
+        }
+
+        return a.e.v < b.e.v;
+    }
+
+    return a.e.u < b.e.u;
+}
+
 bool areEdgesExtremesEquals (Edge e, Edge f) {
     return (minmax(e.u, e.v) == minmax(f.u, f.v));
-}
-
-bool readInput (unsigned int * n, vector <Edge> * E, vector <ConflictingPair> * S, char * inputFilePath) {
-    ifstream inputFileStream(inputFilePath, ifstream::in);
-
-    if (!inputFileStream.is_open()) {
-        return false;
-    }
-
-    string line;
-
-    /* Linhas de comentário, iniciadas com o caractere ‘#’. */
-    do {
-        getline(inputFileStream, line);
-    } while (line[0] == '#');
-
-    /* Uma linha com o nome do arquivo. */
-
-    unsigned int m, p;
-
-    /* Uma linha com o número de vértices do grafo (n). */
-    inputFileStream >> (*n);
-
-    /* Uma linha com o número de arestas do grafo (m). */
-    inputFileStream >> m;
-
-    /* Uma linha com a quantidade de pares de arestas conflitantes (p). */
-    inputFileStream >> m;
-
-    /* Uma linha com a quantidade de pares de arestas conflitantes (p). */
-    inputFileStream >> p;
-
-    (*E) = vector <Edge> (m);
-    (*S) = vector <ConflictingPair> (p);
-
-    /* m linhas, cada uma identificando uma aresta e = (u, v), de custo ce, sendo 0 ≤ u, */
-    /* v ≤ n − 1 e 0 ≤ ce ≤ 500, no seguinte formato: u v ce */
-    for (unsigned int j = 0; j < m; j++) {
-        inputFileStream >> (*E)[j].u >> (*E)[j].v >> (*E)[j].w;
-    }
-
-    /* p linhas, cada uma identificando um par de arestas conflitantes {e, f}, sendo */
-    /* e = (u, v), f = (x, y) e 0 ≤ u, v, x, y ≤ n − 1, no seguinte formato: u v x y */
-    for (unsigned int j = 0; j < p; j++) {
-        inputFileStream >> (*S)[j].e.u >> (*S)[j].e.v >> (*S)[j].f.u >> (*S)[j].f.v;
-
-        for (vector <Edge>::iterator it = (*E).begin(); it != (*E).end(); it++) {
-            if (areEdgesExtremesEquals((*S)[j].e, (*it))) {
-                (*S)[j].e.w = it->w;
-            }
-
-            if (areEdgesExtremesEquals((*S)[j].f, (*it))) {
-                (*S)[j].f.w = it->w;
-            }
-        }
-    }
-
-    return true;
-}
-
-bool readResult (char * resultFilePath, unsigned int n, double * bestDualBoundValue, 
-        int * bestDualBoundIteration, int * totalIterations, double * bestPrimalBoundValue, 
-        int * bestPrimalBoundIteration, vector <Edge> * bestPrimalSolution) {
-    ifstream resultFileStream(resultFilePath, ifstream::in);
-
-    if (!resultFileStream.is_open()) {
-        return false;
-    }
-
-    (*bestPrimalSolution) = vector <Edge> (n - 1);
-
-    /* Uma linha com o melhor limitante dual encontrado impresso com 6 casas decimais. */
-    resultFileStream >> (*bestDualBoundValue);
-
-    /* Uma linha com a iteração do método do subgradiente (MS) */
-    /* em que o melhor limitante dual */
-    /* foi encontrado – considere que as iterações do MS são numeradas a partir de 0. */
-    resultFileStream >> (*bestDualBoundIteration);
-
-    /* Uma linha com a quantidade de iterações do MS executadas. */
-    resultFileStream >> (*totalIterations);
-
-    /* Uma linha com o melhor limitante primal encontrado. */
-    resultFileStream >> (*bestPrimalBoundValue);
-
-    /* Uma linha com a iteração do MS em que o melhor */
-    /* limitante primal foi encontrado – se o */
-    /* melhor limitante primal encontrado foi calculado antes da execução do MS, */
-    /* o número −1 deve ser impresso nesta linha. */
-    resultFileStream >> (*bestPrimalBoundIteration);
-
-    /* n−1 linhas, sendo n o número de vértices do grafo da instância de teste, */
-    /* representando a árvore geradora correspondente ao melhor limitante primal encontrado, */
-    /* cada linha identificando uma aresta e = (u, v) presente na árvore, */
-    /* sendo 0 ≤ u, v ≤ n − 1, no seguinte formato: u v */
-    for (unsigned int i = 0; i < n - 1; i++) {
-        resultFileStream >> (*bestPrimalSolution)[i].u >> (*bestPrimalSolution)[i].v;
-    }
-
-    return true;
 }
 
 void SETSinit (Sets * S, unsigned int n) {
@@ -184,7 +113,7 @@ void SETSunion (Sets * S, unsigned int x, unsigned int y) {
     SETSlink (S, SETSfind (S, x), SETSfind (S, y));
 }
 
-Sets connectedComponents (unsigned int n, vector <Edge> E) {
+Sets connectedComponents (unsigned int n, set <Edge, bool (*) (Edge, Edge)> E) {
     Sets S;
 
     SETSinit(&S, n);
@@ -195,7 +124,7 @@ Sets connectedComponents (unsigned int n, vector <Edge> E) {
     }
 
     /* For each edge (u, v) ∈ E */
-    for (vector <Edge>::iterator it = E.begin(); it != E.end(); it++) {
+    for (set <Edge>::iterator it = E.begin(); it != E.end(); it++) {
         Edge e = (*it);
 
         if (SETSfind(&S, e.u) != SETSfind(&S, e.v)) {
@@ -214,45 +143,224 @@ bool sameComponent (Sets S, unsigned int u, unsigned int v) {
     return false;
 }
 
-bool isFeasible (unsigned int n, vector <ConflictingPair> S, vector <Edge> solution) {
-    if (solution.size() != n - 1) {
-        return false;
-    }
+bool isConnected (unsigned int n, set <Edge, bool (*) (Edge, Edge)> E) {
+    bool result = true;
 
-    Sets components = connectedComponents(n, solution);
+    Sets components = connectedComponents(n, E);
 
-    for (unsigned int u = 0; u < n; u++) {
-        for (unsigned int v = u + 1; v < n; v++) {
+    for (unsigned int u = 0; u < n - 1 && result; u++) {
+        for (unsigned int v = u + 1; v < n && result; v++) {
             if (!sameComponent(components, u, v)) {
-                SETSdestroy (&components);
-                return false;
+                result = false;
             }
         }
     }
 
-    SETSdestroy (&components);
+    SETSdestroy(&components);
 
-    for (vector <ConflictingPair>::iterator it = S.begin(); it != S.end(); it++) {
+    return result;
+}
+
+bool readInput (unsigned int * n, set <Edge, bool (*) (Edge, Edge)> * E, 
+        set <ConflictingPair,bool (*) (ConflictingPair, ConflictingPair)> * S, 
+        char * inputFilePath) {
+    ifstream inputFileStream(inputFilePath, ifstream::in);
+
+    if (!inputFileStream.is_open()) {
+        return false;
+    }
+
+    string line;
+
+    /* Linhas de comentário, iniciadas com o caractere ‘#’. */
+    do {
+        getline(inputFileStream, line);
+    } while (line[0] == '#');
+
+    /* Uma linha com o nome do arquivo. */
+
+    unsigned int m, p;
+
+    /* Uma linha com o número de vértices do grafo (n). */
+    inputFileStream >> (*n);
+
+    /* Uma linha com o número de arestas do grafo (m). */
+    inputFileStream >> m;
+
+    /* Uma linha com a quantidade de pares de arestas conflitantes (p). */
+    inputFileStream >> p;
+
+    bool (*edgeComparatorPointer) (Edge, Edge) = edgeComparator;
+    (*E) = set <Edge, bool (*) (Edge, Edge)> (edgeComparatorPointer);
+
+    bool (*conflictingPairComparatorPointer) (ConflictingPair, ConflictingPair) = 
+        conflictingPairComparator;
+    (*S) = set <ConflictingPair, bool (*) (ConflictingPair, ConflictingPair)> (
+            conflictingPairComparatorPointer);
+
+    /* m linhas, cada uma identificando uma aresta e = (u, v), de custo ce, sendo 0 ≤ u, */
+    /* v ≤ n − 1 e 0 ≤ ce ≤ 500, no seguinte formato: u v ce */
+    for (unsigned int j = 0; j < m; j++) {
+        int u, v;
+        double w;
+
+        inputFileStream >> u >> v >> w;
+
+        Edge e;
+
+        if (u < v) {
+            e.u = u;
+            e.v = v;
+        } else {
+            e.u = v;
+            e.v = u;
+        }
+
+        e.w = w;
+
+        (*E).insert(e);
+    }
+
+    /* p linhas, cada uma identificando um par de arestas conflitantes {e, f}, sendo */
+    /* e = (u, v), f = (x, y) e 0 ≤ u, v, x, y ≤ n − 1, no seguinte formato: u v x y */
+    for (unsigned int j = 0; j < p; j++) {
+        Edge e, f;
+        unsigned int u, v;
+
+        inputFileStream >> u >> v;
+
+        if (u < v) {
+            e.u = u;
+            e.v = v;
+        } else {
+            e.u = v;
+            e.v = u;
+        }
+
+        inputFileStream >> u >> v;
+
+        if (u < v) {
+            f.u = u;
+            f.v = v;
+        } else {
+            f.u = v;
+            f.v = u;
+        }
+
+        set <Edge, bool (*) (Edge, Edge)> :: iterator it = (*E).find(e);
+
+        if (it != (*E).end()) {
+            e.w = it->w;
+        }
+
+        it = (*E).find(f);
+
+        if (it != (*E).end()) {
+            f.w = it->w;
+        }
+
+        ConflictingPair cp;
+
+        if (edgeComparator(e, f)) {
+            cp.e = e;
+            cp.f = f;
+        } else {
+            cp.e = f;
+            cp.f = e;
+        }
+
+        (*S).insert(cp);
+    }
+
+    return true;
+}
+
+bool isFeasible (unsigned int n, 
+        set <ConflictingPair, bool (*) (ConflictingPair, ConflictingPair)> S, 
+        set <Edge, bool (*) (Edge, Edge)> solution) {
+    if (solution.size() != n - 1) {
+        return false;
+    }
+
+    if (!isConnected(n, solution)) {
+        return false;
+    }
+
+    for (set <ConflictingPair>::iterator it = S.begin(); it != S.end(); it++) {
         unsigned int counter = 0;
 
-        for (vector <Edge>::iterator it2 = solution.begin(); it2 != solution.end(); it2++) {
-            if (areEdgesExtremesEquals(it->e, (*it2)) || areEdgesExtremesEquals(it->f, (*it2))) {
-                counter++;
-            }
+        set <Edge>::iterator it2 = solution.find(it->e);
 
-            if (counter >= 2) {
-                return false;
-            }
+        if (it2 != solution.end()) {
+            counter++;
+        }
+
+        it2 = solution.find(it->f);
+
+        if (it2 != solution.end()) {
+            counter++;
+        }
+
+        if (counter >= 2) {
+            return false;
         }
     }
 
     return true;
 }
 
-bool isResultValid (unsigned int n, vector <Edge> E, vector <ConflictingPair> S, 
+bool readResult (char * resultFilePath, unsigned int n, double * bestDualBoundValue, 
+        int * bestDualBoundIteration, int * totalIterations, double * bestPrimalBoundValue, 
+        int * bestPrimalBoundIteration, set <Edge, bool (*) (Edge, Edge)> * bestPrimalSolution) {
+    ifstream resultFileStream(resultFilePath, ifstream::in);
+
+    if (!resultFileStream.is_open()) {
+        return false;
+    }
+
+    bool (*edgeComparatorPointer) (Edge, Edge) = edgeComparator;
+    (*bestPrimalSolution) = set <Edge, bool (*) (Edge, Edge)> (edgeComparatorPointer);
+
+    /* Uma linha com o melhor limitante dual encontrado impresso com 6 casas decimais. */
+    resultFileStream >> (*bestDualBoundValue);
+
+    /* Uma linha com a iteração do método do subgradiente (MS) */
+    /* em que o melhor limitante dual */
+    /* foi encontrado – considere que as iterações do MS são numeradas a partir de 0. */
+    resultFileStream >> (*bestDualBoundIteration);
+
+    /* Uma linha com a quantidade de iterações do MS executadas. */
+    resultFileStream >> (*totalIterations);
+
+    /* Uma linha com o melhor limitante primal encontrado. */
+    resultFileStream >> (*bestPrimalBoundValue);
+
+    /* Uma linha com a iteração do MS em que o melhor */
+    /* limitante primal foi encontrado – se o */
+    /* melhor limitante primal encontrado foi calculado antes da execução do MS, */
+    /* o número −1 deve ser impresso nesta linha. */
+    resultFileStream >> (*bestPrimalBoundIteration);
+
+    /* n−1 linhas, sendo n o número de vértices do grafo da instância de teste, */
+    /* representando a árvore geradora correspondente ao melhor limitante primal encontrado, */
+    /* cada linha identificando uma aresta e = (u, v) presente na árvore, */
+    /* sendo 0 ≤ u, v ≤ n − 1, no seguinte formato: u v */
+    for (unsigned int i = 0; i < n - 1; i++) {
+        Edge e;
+
+        resultFileStream >> e.u >> e.v;
+
+        (*bestPrimalSolution).insert(e);
+    }
+
+    return true;
+}
+
+bool isResultValid (unsigned int n, set <Edge, bool (*) (Edge, Edge)> E, 
+        set <ConflictingPair, bool (*) (ConflictingPair, ConflictingPair)> S, 
         double bestDualBoundValue, int bestDualBoundIteration, int totalIterations, 
         double bestPrimalBoundValue, int bestPrimalBoundIteration, 
-        vector <Edge> bestPrimalSolution) {
+        set <Edge, bool (*) (Edge, Edge)> bestPrimalSolution) {
     if (bestDualBoundIteration > totalIterations) {
         return false;
     }
@@ -270,16 +378,17 @@ bool isResultValid (unsigned int n, vector <Edge> E, vector <ConflictingPair> S,
     }
 
     double bestPrimalSolutionValue = 0.0;
-    
-    for (vector <Edge>::iterator it = bestPrimalSolution.begin(); it != bestPrimalSolution.end(); 
-            it++) {
-        bool found = false;
 
-        for (vector <Edge>::iterator it2 = E.begin(); it2 != E.end() && !found; it2++) {
-            if (areEdgesExtremesEquals((*it), (*it2))) {
-                bestPrimalSolutionValue += it2->w;
-                found = true;
-            }
+    for (set <Edge>::iterator it = bestPrimalSolution.begin(); it != bestPrimalSolution.end(); 
+            it++) {
+        Edge e = (*it);
+
+        set <Edge>::iterator it2 = E.find(e);
+
+        if (it2 != E.end()) {
+            Edge f = (*it2);
+
+            bestPrimalSolutionValue += f.w;
         }
     }
 
@@ -315,8 +424,8 @@ int main (int argc, char * argv[]) {
     }
 
     unsigned int n;
-    vector <Edge> E;
-    vector <ConflictingPair> S;
+    set <Edge, bool (*) (Edge, Edge)> E;
+    set <ConflictingPair,bool (*) (ConflictingPair, ConflictingPair)> S;
 
     if (!readInput(&n, &E, &S, argv[1])) {
         cerr << "Error while reading input!" << endl;
@@ -324,7 +433,7 @@ int main (int argc, char * argv[]) {
 
     double bestDualBoundValue, bestPrimalBoundValue;
     int bestDualBoundIteration, totalIterations, bestPrimalBoundIteration;
-    vector <Edge> bestPrimalSolution;
+    set <Edge, bool (*) (Edge, Edge)> bestPrimalSolution;
 
     if (!readResult(argv[2], n, &bestDualBoundValue, &bestDualBoundIteration, &totalIterations, 
                 &bestPrimalBoundValue, &bestPrimalBoundIteration, &bestPrimalSolution)) {
